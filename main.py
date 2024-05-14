f8f226d7f0dfc2f799f8c0b723181217816f27ac
Bruno Mendes <bfmendesadm@gmail.com>
2024-05-14 11:39:15 -0300

    melhorias ao código

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import QUrl, Qt
 from PyQt5.QtGui import QDesktopServices
+from raspagem import WebScraping
 
 class MeuApp(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi('interface.ui', self)
+
+        # Objeto de Raspagem
+        self.raspagem = WebScraping()
         
         # Inicializar o índice da notícia atual com 0
         self.indice_noticia_atual = 0
@@ -17,10 +21,10 @@ class MeuApp(QMainWindow):
         self.btnProximo.clicked.connect(self.btn_Proximo)
         self.btnAnterior.clicked.connect(self.btn_Anterior)
         self.btnFechar.clicked.connect(self.btn_Fechar)
-        self.btnHistoria.clicked.connect(self.btn_Historia)
-        self.btnHome.clicked.connect(self.btn_Home)
-        self.btnJogadores.clicked.connect(self.btn_Jogadores) 
-        self.btnNoticias.clicked.connect(self.btn_Noticias)
+        self.btnHome.clicked.connect(self.showHome)
+        self.btnHistoria.clicked.connect(self.showHistoria)
+        self.btnLerNoticias.clicked.connect(self.showLerNoticia)
+        self.btnNoticia.clicked.connect(self.showNoticias)
 
         # Gerar os cards de notícias e exibir a primeira notícia
         self.listaDeCards = self.gerarCardsDeNoticias()
@@ -29,51 +33,18 @@ class MeuApp(QMainWindow):
         # Conectar o botão para abrir o link da notícia
         self.btnLink.clicked.connect(self.abrirLink)
 
-        # Deixa a interface transparente
+        # Deixa a interface (própria do Qt) transparente
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-                              
-    # Função para procurar notícias no site
-    def procura_site(self, tag, classe):
-        url = 'https://ge.globo.com/futebol/futebol-internacional/futebol-espanhol/times/real-madrid/'
-        requisicao  = requests.get(url)
-        pagina = BeautifulSoup(requisicao.text, 'html.parser')
-        lista_noticias = pagina.find_all(tag, class_= classe)
-        return lista_noticias
-    
-    # Gerar os cards de notícias
-    def gerarCardsDeNoticias(self):
-        cards = [] 
-        for card in self.get_noticias():
-            titulo, resumo, link = self.get_detalhes_noticia(card)
-            cards.append((titulo, resumo, link))
-        return cards
-    
-    # Obter notícias do site
-    def get_noticias(self):
-        return self.procura_site('div', 'feed-post-body')
-    
-    # Obter detalhes (título, resumo e link) de uma notícia
-    def get_detalhes_noticia(self, card):
-        titulo_link = card.find_all('a', class_= 'feed-post-link')[0]
-        link = titulo_link.get('href')
-        titulo = titulo_link.find_all('p')[0].getText()
-        resumo = self.get_resumo(card)
-        return titulo, resumo, link
-    
-    # Obter o resumo de uma notícia
-    def get_resumo(self, card):
-        texto = card.find_all('div', class_= 'feed-post-body-resumo')[0].text
-        return texto
-    
+
     # Exibir a notícia atual no aplicativo
     def exibirNoticiaAtual(self):
         noticia_atual = self.listaDeCards[self.indice_noticia_atual]
         titulo, resumo, link = noticia_atual
         self.setTitulo(titulo)  
         self.setResumo(resumo)  
-        self.setLink(link)     
-    
+        self.setLink(link)   
+                                
     # Atualizar a exibição para a próxima notícia
     def btn_Proximo(self):
         self.indice_noticia_atual += 1
@@ -106,25 +77,25 @@ class MeuApp(QMainWindow):
         if hasattr(self, 'link_noticia'):
             QDesktopServices.openUrl(QUrl(self.link_noticia))
 
+    # Função para mostrar as notícias
+    def btnNoticias(self):
+        texto = self.raspagem.get_noticiasFull(self.link_noticias)
+    
     # Função para fechar a janela
     def btn_Fechar(self):
         self.close()
 
-    # Função para mostrar a história
-    def btn_Historia(self):
-        pass
+    def showHome(self):
+        self.stackedWidget.setCurrentIndex(0)
 
-    # Função para mostrar a página inicial
-    def btn_Home(self):
-        pass
+    def showHistoria(self):
+        self.stackedWidget.setCurrentIndex(1)
 
-    # Função para mostrar informações sobre os Melhores jogadores
-    def btn_Jogadores(self):
-        pass
+    def showLerNoticia(self):
+        self.stackedWidget.setCurrentIndex(2)
 
-    # Função para mostrar as notícias
-    def btn_Noticias(self):
-        pass
+    def showNoticias(self):
+        self.stackedWidget.setCurrentIndex(3)
 
 if __name__ == '__main__':
     app = QApplication([])
---
+++ raspagem.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class WebScraping:
+
+    # Função para procurar notícias no site
+    def procura_site(self, url, tag, classe):
+        requisicao  = requests.get(url)
+        pagina = BeautifulSoup(requisicao.text, 'html.parser')
+        lista_noticias = pagina.find_all(tag, class_= classe)
+        return lista_noticias
+    
+    # Gerar os cards de notícias
+    def gerarCardsDeNoticias(self):
+        cards = [] 
+        for card in self.get_noticias():
+            titulo, resumo, link = self.get_detalhes_noticia(card)
+            cards.append((titulo, resumo, link))
+        return cards
+    
+    # Obter notícias do site
+    def get_noticias(self):
+        url = 'https://ge.globo.com/futebol/futebol-internacional/futebol-espanhol/times/real-madrid/'
+        return self.procura_site(url, 'div', 'feed-post-body')
+    
+    # Obter detalhes (título, resumo e link) de uma notícia
+    def get_detalhes_noticia(self, card):
+        titulo_link = card.find_all('a', class_= 'feed-post-link')[0]
+        self.link = titulo_link.get('href')
+        titulo = titulo_link.find_all('p')[0].getText()
+        resumo = self.get_resumo(card)
+        return titulo, resumo, self.link
+    
+    # Obter o resumo de uma notícia
+    def get_resumo(self, card):
+        texto = card.find_all('div', class_= 'feed-post-body-resumo')[0].text
+        return texto
+
+    # Função de web scrap da noticias inteira
+    def get_noticiasFull(self, url):
+        requisicao  = requests.get(url)
+        pagina = BeautifulSoup(requisicao.text, 'html.parser')
+        textos = [p.get_text() for p in pagina.find_all('p', 'content-text__container')]
+        textos = str(textos).replace('+', '\n-').replace("', '", "").replace("['","").replace("']","")
+        return textos
